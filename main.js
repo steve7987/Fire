8cf4f396c0d9dfbe17c32fe848e8ab1a3740e412
steve7987 <steve.7987@gmail.com>
2014-12-04 10:13:51 +0900

    Added Collision Adjustment
    When colliding with blocks, hero should now adjust position to be where
    it first touched the block.

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ function Hero(xpos, ypos, width, height, color){
 	this.xvel = 0;
 	this.yvel = 0;
 	this.speed = 150;
-	this.jumpPower = 400;
+	this.jumpPower = 250;
 }
 
 Hero.prototype.draw = function(){
@@ -112,21 +112,53 @@ Hero.prototype.draw = function(){
 Hero.prototype.update = function(dt){
 	var oldX = this.x;
 	var oldY = this.y;
-	
-	this.x += this.xvel * dt / 1000.0;
-	this.y += this.yvel * dt / 1000.0;
 	//gravity
 	this.yvel += 150 * dt / 1000.0;
+	//update position
+	this.x += this.xvel * dt / 1000.0;
+	this.y += this.yvel * dt / 1000.0;
+
 	for (var i = 0; i < blockList.length; i++){
 		if (blockList[i].collide(this)){
-			this.x = oldX;
-			this.y = oldY;
-			this.xvel = 0;
-			this.yvel = 0;
+			//compute new position and velocities
+			this.adjust(blockList[i], oldX, oldY);
 		}
 	}
-	
+}
 
+Hero.prototype.adjust = function(cBlock, oldX, oldY){
+	var tRight = (cBlock.x - oldX - this.dx) / this.xvel;
+	var tLeft = (cBlock.x + cBlock.dx - oldX) / this.xvel;
+	var tUp = (cBlock.y +cBlock.dy - oldY) / this.yvel;
+	var tDown = (cBlock.y - oldY - this.dy) / this.yvel;
+	if (tRight < 0 || this.xvel == 0) { tRight = Infinity; }
+	if (tLeft < 0 || this.xvel == 0) { tLeft = Infinity; }
+	if (tUp < 0 || this.yvel == 0) { tUp = Infinity; }
+	if (tDown < 0 || this.yvel == 0) { tDown = Infinity; }
+	console.log(tRight + ", " + tLeft + ", " + tUp + ", " + tDown);
+	if (tRight < tLeft && tRight < tUp && tRight < tDown){
+		this.x = cBlock.x - this.dx; //oldX + this.xvel * tRight;
+		this.y = oldY + this.yvel * tRight;
+		this.xvel = 0;
+	}
+	else if (tLeft < tUp && tLeft < tDown){
+		this.x = cBlock.x + cBlock.dx; //oldX + this.xvel * tLeft;
+		this.y = oldY + this.yvel * tLeft;
+		this.xvel = 0;
+	}
+	else if (tUp < tDown){
+		this.x = oldX + this.xvel * tUp;
+		this.y = cBlock.y + cBlock.dy; //oldY + this.yvel * tUp;
+		this.yvel = 0;
+	}
+	else {
+		this.x = oldX + this.xvel * tDown;
+		this.y = cBlock.y - this.dy; //oldY + this.yvel * tDown;
+		this.yvel = 0;
+	}
+	if (cBlock.collide(this)){
+		console.log("still colliding");
+	}
 }
 
 var hero = new Hero(30, canvas.height / 2, 8, 20, '#1FFF1F');
@@ -146,10 +178,10 @@ Block.prototype.draw = function(){
 }
 
 Block.prototype.collide = function(target){
-	return !(this.x > target.x + target.dx || 
-           this.x + this.dx < target.x || 
-           this.y > target.y + target.dy ||
-           this.y + this.dy < target.y);
+	return !(this.x >= target.x + target.dx || 
+           this.x + this.dx <= target.x || 
+           this.y >= target.y + target.dy ||
+           this.y + this.dy <= target.y);
 }
 
 var blockList = [];
@@ -169,13 +201,12 @@ var draw = function(timestamp){
 	ctx.fillStyle = "#000000";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	ctx.fill();
-	
-
+	//draw items
 	for (var i = 0; i < blockList.length; i++){
 		blockList[i].draw();
 	}
-
 	hero.draw();
+	//update items
 	update(timestamp - lastFrameTime);
 	lastFrameTime = timestamp;
 }
